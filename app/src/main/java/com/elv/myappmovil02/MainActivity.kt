a99400c8df53b0b14eefce42ca84cfe564dabceb
Edwin Leon <eleonv@contraloria.gob.pe>
2025-05-17 22:35:34 -0500

    consumo api completado

--- app/src/main/java/com/elv/myappmovil02/MainActivity.kt
+++ app/src/main/java/com/elv/myappmovil02/MainActivity.kt
@@ -2,18 +2,14 @@ package com.elv.myappmovil02
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
-import com.elv.myappmovil02.hilt.ProductService
+//import com.elv.myappmovil02.hilt.ProductService
 import com.elv.myappmovil02.hilt.ProductViewModel
 import com.elv.myappmovil02.ui.theme.MyAppMovil02Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,7 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val roductViewModel: ProductViewModel by viewModels()
+    private val productViewModel: ProductViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +28,7 @@ class MainActivity : ComponentActivity() {
         //productViewModel = ViewModelProvider(this, viewModelFactory)[ProductViewModel::class.java]
         // --- Fin de la creación manual ---
 
-        roductViewModel.onCreate()
+        productViewModel.onCreate()
 
 
 
